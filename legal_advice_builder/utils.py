e6acedc43f675ccd78c61aa671860cedff2a6fe0
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-06-25 09:10:57 +0200

    Replace bleach with nh3

--- legal_advice_builder/utils.py
+++ legal_advice_builder/utils.py
@@ -1,8 +1,6 @@
 import datetime
 
-import bleach
-from bleach.css_sanitizer import CSSSanitizer
-
+import nh3
 
 
 def generate_answers_dict_for_template(answers):
@@ -10,13 +8,13 @@ def generate_answers_dict_for_template(answers):
 
     answers_dict = {}
     for answer in answers:
-        question = Question.objects.get(id=answer.get('question'))
-        option = answer.get('option')
-        text = answer.get('text')
-        date = answer.get('date')
+        question = Question.objects.get(id=answer.get("question"))
+        option = answer.get("option")
+        text = answer.get("text")
+        date = answer.get("date")
         if date:
             try:
-                date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+                date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
             except ValueError:
                 pass
         key, value = question.get_dict_key(option, text, date)
@@ -24,29 +22,50 @@ def generate_answers_dict_for_template(answers):
     return answers_dict
 
 
-def clean_html_field(text, setting='default'):
-    allowed_tags = ['p', 'strong', 'em',
-                    'u', 'ol', 'li', 'ul', 'h1',
-                    'h2', 'h3', 'h4', 'h5']
-    allowed_attrs = {'*': ['style']}
-    css_sanitizer = CSSSanitizer(allowed_css_properties=['text-align'])
+def only_text_align_css_properties(tag, attr, value):
+    """Poor CSS parser to check if the value is a text-align property."""
+    if attr == "style":
+        props = value.split(";")
+        for prop in props:
+            if not prop.strip().startswith("text-align:"):
+                return None
 
-    return bleach.clean(text,
-                        tags=allowed_tags,
-                        attributes=allowed_attrs,
-                        css_sanitizer=css_sanitizer,
-                        strip=True)
+    return value
+
+
+def clean_html_field(text, setting="default"):
+    allowed_tags = [
+        "p",
+        "strong",
+        "em",
+        "u",
+        "ol",
+        "li",
+        "ul",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+    ]
+    allowed_attrs = {"*": ["style"]}
+    return nh3.clean(
+        text,
+        tags=allowed_tags,
+        attributes=allowed_attrs,
+        attribute_filter=only_text_align_css_properties,
+    )
 
 
 def get_answer_from_list(answers, question):
     for answer in answers:
-        if answer.get('question') == str(question.id):
-            option = answer.get('option')
-            text = answer.get('text')
-            date = answer.get('date')
+        if answer.get("question") == str(question.id):
+            option = answer.get("option")
+            text = answer.get("text")
+            date = answer.get("date")
             if date:
                 try:
-                    date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+                    date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
                 except ValueError:
                     pass
             return text or date or option
--- setup.py
+++ setup.py
@@ -1,30 +1,29 @@
 import re
 
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
 # get metadata from mudule using a regexp
-with open('legal_advice_builder/__init__.py') as f:
-    metadata = dict(re.findall(r'__(.*)__ = [\']([^\']*)[\']', f.read()))
+with open("legal_advice_builder/__init__.py") as f:
+    metadata = dict(re.findall(r"__(.*)__ = [\']([^\']*)[\']", f.read()))
 
 setup(
-    name=metadata['title'],
-    version=metadata['version'],
-    author=metadata['author'],
-    author_email=metadata['email'],
-    maintainer=metadata['author'],
-    maintainer_email=metadata['email'],
-    license=metadata['license'],
-    url='https://github.com/OpenLegalTech',
+    name=metadata["title"],
+    version=metadata["version"],
+    author=metadata["author"],
+    author_email=metadata["email"],
+    maintainer=metadata["author"],
+    maintainer_email=metadata["email"],
+    license=metadata["license"],
+    url="https://github.com/OpenLegalTech",
     install_requires=[
-        'Django',
-        'django-treebeard',
-        'WeasyPrint',
-        'Markdown',
-        'bleach',
-        'django-tinymce',
-        'htmldocx'
+        "Django",
+        "django-treebeard",
+        "WeasyPrint",
+        "Markdown",
+        "nh3",
+        "django-tinymce",
+        "htmldocx",
     ],
     packages=find_packages(),
-    include_package_data=True
+    include_package_data=True,
 )
