46c5c3cb92a4c5995b2b78aad5e8f70d8d19d310
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-06-25 09:10:34 +0200

    Format and move docx/weasyprint import into call sites

--- legal_advice_builder/mixins.py
+++ legal_advice_builder/mixins.py
@@ -1,19 +1,14 @@
 import io
 
-import weasyprint as wp
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-from docx import Document
-from htmldocx import HtmlToDocx
 
-from .models import Answer
-from .models import Question
+from .models import Answer, Question
 from .signals import answer_created
 from .utils import get_answer_from_list
 
 
 class GenrateFormWizardMixin:
-
     def get_initial_dict(self):
         return {}
 
@@ -27,53 +22,55 @@ class GenrateFormWizardMixin:
 
     def get_initial_data(self, question):
         question_data = self._get_dict_entry_for_question(question)
-        if question_data and 'initial' in question_data:
+        if question_data and "initial" in question_data:
             text_types = [question.TEXT, question.SINGLE_LINE]
             single_option_types = [question.SINGLE_OPTION, question.YES_NO]
             if question.field_type in text_types:
-                return {'text': question_data.get('initial')}
+                return {"text": question_data.get("initial")}
             elif question.field_type in single_option_types:
-                return {'option': question_data.get('initial')}
+                return {"option": question_data.get("initial")}
             elif question.field_type == question.MULTIPLE_OPTIONS:
-                return {'option': question_data.get('initial')}
+                return {"option": question_data.get("initial")}
             elif question.field_type == question.DATE:
-                return {'date': question_data.get('initial')}
+                return {"date": question_data.get("initial")}
 
     def get_initial_options(self, question):
         question_data = self._get_dict_entry_for_question(question)
-        if question_data and 'options' in question_data:
-            return question_data.get('options')
+        if question_data and "options" in question_data:
+            return question_data.get("options")
         return question.options
 
     def render_next(self, question, answers, initial_data=None):
-        self.storage.set_data({
-            'current_questionaire': question.questionaire.id,
-            'current_question': question.id,
-            'answers': answers
-        })
+        self.storage.set_data(
+            {
+                "current_questionaire": question.questionaire.id,
+                "current_question": question.id,
+                "answers": answers,
+            }
+        )
         initial_options = self.get_initial_options(question)
         if not initial_data:
             initial_data = self.get_initial_data(question)
-        form = self.get_form(question=question,
-                             initial_data=initial_data,
-                             options=initial_options)
+        form = self.get_form(
+            question=question, initial_data=initial_data, options=initial_options
+        )
         return self.render_form(form)
 
     def get_current_question(self):
-        question_id = self.storage.get_data().get('current_question')
+        question_id = self.storage.get_data().get("current_question")
         return Question.objects.get(id=question_id)
 
-    def get_answer_for_question(self, question_short_title, questionaire_short_title=None):
-        filters = {
-            'short_title': question_short_title
-        }
+    def get_answer_for_question(
+        self, question_short_title, questionaire_short_title=None
+    ):
+        filters = {"short_title": question_short_title}
         if questionaire_short_title:
-            filters['questionaire__short_title'] = questionaire_short_title
+            filters["questionaire__short_title"] = questionaire_short_title
         questions = Question.objects.filter(**filters)
         if questions:
-            answers = self.storage.get_data().get('answers')
+            answers = self.storage.get_data().get("answers")
             question = questions.first()
-            answers = self.storage.get_data().get('answers')
+            answers = self.storage.get_data().get("answers")
             if answers:
                 return get_answer_from_list(answers, question)
         return ""
@@ -81,10 +78,10 @@ class GenrateFormWizardMixin:
     def get_form(self, question=None, data=None, initial_data=None, options=None):
         form_class = self.wizard_form_class
         form_kwargs = {
-            'question': question,
-            'data': data,
-            'initial': initial_data,
-            'options': options
+            "question": question,
+            "data": data,
+            "initial": initial_data,
+            "options": options,
         }
         return form_class(**form_kwargs)
 
@@ -95,23 +92,26 @@ class GenrateFormWizardMixin:
 
     def validate_form_and_get_next(self, question=None, answers=None, data=None):
         initial_options = self.get_initial_options(question)
-        question_form = self.get_form(question=question,
-                                      data=self.request.POST,
-                                      options=initial_options)
+        question_form = self.get_form(
+            question=question, data=self.request.POST, options=initial_options
+        )
         if question_form.is_valid():
             cleaned_data = question_form.cleaned_data
             status = question.get_status(
-                option=cleaned_data.get('option'),
-                text=cleaned_data.get('text'),
-                date=cleaned_data.get('date'))
-            next_question = status.get('next')
+                option=cleaned_data.get("option"),
+                text=cleaned_data.get("text"),
+                date=cleaned_data.get("date"),
+            )
+            next_question = status.get("next")
             answers = answers + [cleaned_data]
-            if not status.get('ongoing'):
-                self.storage.set_data({
-                    'current_questionaire': question.questionaire.id,
-                    'current_question': question.id,
-                    'answers': answers
-                })
+            if not status.get("ongoing"):
+                self.storage.set_data(
+                    {
+                        "current_questionaire": question.questionaire.id,
+                        "current_question": question.id,
+                        "answers": answers,
+                    }
+                )
                 return self.render_status(**status)
             elif next_question:
                 return self.render_next(next_question, answers)
@@ -130,15 +130,11 @@ class GenrateFormWizardMixin:
             answer = self.save_answers(answers)
             form = self.get_answer_template_form(answer)
             preview = answer.rendered_document
-            context.update({
-                'answer_form': form,
-                'preview': preview
-            })
+            context.update({"answer_form": form, "preview": preview})
         return self.render_to_response(context)
 
 
 class GenerateEditableDocumentMixin:
-
     def save_answers(self, answers):
         answer = Answer.objects.create(
             law_case=self.get_lawcase(),
@@ -161,11 +157,9 @@ class GenerateEditableDocumentMixin:
             answer_form.save()
         preview = answer.rendered_document
         context = self.get_context_data(**kwargs)
-        context.update({
-            'answer': answer,
-            'answer_form': answer_form,
-            'preview': preview
-        })
+        context.update(
+            {"answer": answer, "answer_form": answer_form, "preview": preview}
+        )
         return self.render_to_response(context)
 
     def get_answer_template_form(self, answer):
@@ -175,7 +169,6 @@ class GenerateEditableDocumentMixin:
 
 
 class GeneratePDFDownloadMixin:
-
     def get_html_string(self, answers):
         context = self.get_template_with_context(answers)
         return render_to_string(self.download_template_name, context)
@@ -185,19 +178,23 @@ class GeneratePDFDownloadMixin:
         return self.get_context_data(template=template, **kwargs)
 
     def get_filename(self):
-        return 'download'
+        return "download"
 
     def get_pdf_bytes(self, html_string):
+        import weasyprint as wp
+
         doc = wp.HTML(string=html_string)
-        return doc.write_pdf(stylesheets=[
-            wp.CSS(string='body { font-family: sans-serif !important }'),
-            wp.CSS(string='@page { size: A4; margin: 2cm }'),
-            wp.CSS(string='body { font-size: 14px !important }'),
-            wp.CSS(string='body { line-height: 1.5 !important }')
-        ])
+        return doc.write_pdf(
+            stylesheets=[
+                wp.CSS(string="body { font-family: sans-serif !important }"),
+                wp.CSS(string="@page { size: A4; margin: 2cm }"),
+                wp.CSS(string="body { font-size: 14px !important }"),
+                wp.CSS(string="body { line-height: 1.5 !important }"),
+            ]
+        )
 
     def render_pdf_download_response(self, answers, answer=None):
-        html_string = ''
+        html_string = ""
         if answer:
             html_string = answer.rendered_document
         else:
@@ -206,18 +203,19 @@ class GeneratePDFDownloadMixin:
 
     def generate_pdf_download(self, html_string):
         response = HttpResponse(
-            self.get_pdf_bytes(html_string),
-            content_type='application/pdf'
+            self.get_pdf_bytes(html_string), content_type="application/pdf"
         )
         filename = self.get_filename()
-        attachment = 'attachment; filename="{}{}"'.format(filename, '.pdf')
-        response['Content-Disposition'] = attachment
+        attachment = 'attachment; filename="{}{}"'.format(filename, ".pdf")
+        response["Content-Disposition"] = attachment
         return response
 
 
 class GenerateWordDownloadMixin:
-
     def get_word_from_html(self, html_string):
+        from docx import Document
+        from htmldocx import HtmlToDocx
+
         document = Document()
         new_parser = HtmlToDocx()
         new_parser.add_html_to_document(html_string, document)
@@ -235,7 +233,7 @@ class GenerateWordDownloadMixin:
         return render_to_string(self.download_template_name, context)
 
     def render_word_download_response(self, answers, answer=None):
-        html_string = ''
+        html_string = ""
         if answer:
             html_string = answer.rendered_document
         else:
@@ -243,21 +241,20 @@ class GenerateWordDownloadMixin:
         return self.generate_word_download(html_string)
 
     def get_filename(self):
-        return 'download'
+        return "download"
 
     def generate_word_download(self, html_string):
         response = HttpResponse(
             self.get_word_from_html(html_string).read(),
-            content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+            content_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
         )
         filename = self.get_filename()
-        attachment = 'attachment; filename="{}{}"'.format(filename, '.docx')
-        response['Content-Disposition'] = attachment
+        attachment = 'attachment; filename="{}{}"'.format(filename, ".docx")
+        response["Content-Disposition"] = attachment
         return response
 
 
 class GenerateHTMLDownloadMixin:
-
     def get_html(self, html_string):
         return html_string
 
@@ -270,7 +267,7 @@ class GenerateHTMLDownloadMixin:
         return render_to_string(self.download_template_name, context)
 
     def render_html_download_response(self, answers, answer=None):
-        html_string = ''
+        html_string = ""
         if answer:
             html_string = answer.rendered_document
         else:
@@ -278,13 +275,11 @@ class GenerateHTMLDownloadMixin:
         return self.generate_html_download(html_string)
 
     def get_filename(self):
-        return 'download'
+        return "download"
 
     def generate_html_download(self, html_string):
-        response = HttpResponse(
-            self.get_html(html_string)
-        )
+        response = HttpResponse(self.get_html(html_string))
         filename = self.get_filename()
-        attachment = 'attachment; filename="{}{}"'.format(filename, '.html')
-        response['Content-Disposition'] = attachment
+        attachment = 'attachment; filename="{}{}"'.format(filename, ".html")
+        response["Content-Disposition"] = attachment
         return response
